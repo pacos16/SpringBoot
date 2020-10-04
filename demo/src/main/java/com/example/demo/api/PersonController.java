e585db89d5a61940a526c171f8a5c8347fbde088
Paco Signes <pacos16@gmail.com>
2020-10-04 18:32:56 +0200

    Set Up Person

---
+++ demo/src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,19 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    public void addPerson(Person person){
+        personService.addPerson(person);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/dao/FakePersonDataAccesService.java
@@ -0,0 +1,19 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository("fakeDao")
+public class FakePersonDataAccesService implements PersonDao{
+
+    private static List<Person> dB =new ArrayList<>();
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        dB.add(new Person(id,person.getName()));
+        return 1;
+    }
+}
