d3f1e35e96c7faa43e7ffc52278b0008b436aa1a
Paco Signes <pacos16@gmail.com>
2020-10-05 18:08:21 +0200

    Added getAll and getPersonById

--- demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -15,5 +16,27 @@ public class FakePersonDataAccessService implements PersonDao{
     public int insertPerson(UUID id, Person person) {
         dB.add(new Person(id,person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> getAllPeople() {
+        return dB;
+    }
+
+    @Override
+    public Optional<Person> getPersonById(UUID id) {
+         return dB.stream()
+                 .filter(person -> person.getId().equals(id))
+                 .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
     }
 }
