ef6ffabeb0659ff976425d0cc8cff84fd46ce9ad
Paco Signes <pacos16@gmail.com>
2020-10-07 16:07:47 +0200

    stream utilites

--- Extras/src/main/java/JavaStream.java
+++ Extras/src/main/java/JavaStream.java
@@ -1,4 +1,7 @@
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class JavaStream{
@@ -7,17 +10,61 @@ public class JavaStream{
 
     public static void main(String[] args) {
         List<Persona> personas=getPersonas();
-        //Sort
+        //Filter
+        System.out.println("\nFilter");
         List<Persona> mujeres =personas.stream()
                 .filter(persona -> persona.getGenero().equals(Genero.MUJER))
                 .collect(Collectors.toList());
 
         mujeres.forEach(System.out::println);
+
+        //Sort
+        System.out.println("\nSort");
+        personas.stream()
+                .sorted(Comparator.comparing(Persona::getEdad))
+                .forEach(System.out::println);
+
+        //AllMatch
+        System.out.println("\nAllMatch");
+        boolean allMatch=personas.stream().allMatch(persona -> persona.getEdad()<50);
+        System.out.println(allMatch);
+
+        //AnyMatch
+        System.out.println("\nAnyMatch");
+        boolean anyMatch=personas.stream().anyMatch(persona -> persona.getGenero().equals(Genero.HOMBRE));
+        System.out.println(anyMatch);
+
+        //NoneMatch
+        System.out.println("\nNoneMatch");
+        boolean noMatch=personas.stream().anyMatch(persona -> persona.getNombre().equals("Antonieta"));
+        System.out.println(anyMatch);
+
+        //Max
+        System.out.println("\nMax");
+        personas.stream()
+                .max(Comparator.comparing(Persona::getEdad))
+                .ifPresent(System.out::println);
+        //Min
+        System.out.println("\nMin");
+        personas.stream()
+                .min(Comparator.comparing(Persona::getEdad))
+                .ifPresent(System.out::println);
+        //Group
+        System.out.println("\nGroupBy");
+        personas.stream().collect(Collectors.groupingBy(Persona::getGenero))
+                .forEach((genero, personas1) ->{
+                    System.out.println(genero);
+                    personas1.forEach(System.out::println);
+
+                } );
+
+
+
     }
 
 
-
-    public static class Persona{
+    //Class
+    private static class Persona{
 
         private String nombre;
         private int edad;
@@ -49,7 +96,7 @@ public class JavaStream{
     }
 
     public enum Genero{HOMBRE,MUJER}
-
+    //data
     private static List<Persona> getPersonas(){
         return List.of(
                 new Persona("Michael",35,Genero.HOMBRE),
